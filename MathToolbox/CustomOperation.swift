ff886b876c8c12b7dd9eda4093b2680853bf19b0
Sweeper777 <sumulang@gmail.com>
2016-05-10 12:50:09 +0800

    finished the calculate method in CustomOperation

--- MathToolbox/CustomOperation.swift
+++ MathToolbox/CustomOperation.swift
@@ -1,4 +1,5 @@
 import UIKit
+import MathParser
 
 class CustomOperation : Operation {
     let operationEntity: OperationEntity
@@ -17,11 +18,54 @@ class CustomOperation : Operation {
     var operationImage: UIImage? { return nil }
     
     func calculate (inputs: [String: Double]) -> [(name: String, from: String, result: String)]? {
-        // TODO: implement calculate!
-        return nil
+        var result: [(name: String, from: String, result: String)] = []
+        
+        operationEntity.results?.forEach {
+            let item = $0 as! OperationResult
+            var evaluator = Evaluator(caseSensitive: false)
+            evaluator.angleMeasurementMode = .Degrees
+            let x = try? evaluator.evaluate(Expression(string: item.formula!))
+            
+            if x != nil {
+                let froms = findAllStringsInQuotes(string: item.formula!)
+                let from = froms.joinWithSeparator(", ")
+                let hasPi = item.formula!.lowercaseString.containsString("pi") || item.formula!.lowercaseString.containsString("π")
+                result.append((item.name!, from, correctToSigFigAndPi(x!, hasPi)))
+            }
+        }
+        
+        if result.count == 0 {
+            return nil
+        } else {
+            return result
+        }
     }
     
     init(entity: OperationEntity) {
         operationEntity = entity
     }
+}
+
+func findAllStringsInQuotes(string string: String, quoteCharacter: Character = "'") -> [String] {
+    var result = [String]()
+    var shouldAddToCurrent = false
+    var current = ""
+    
+    for char in string.characters {
+        if char == quoteCharacter {
+            if shouldAddToCurrent {
+                shouldAddToCurrent = false
+                result.append(current)
+            } else {
+                shouldAddToCurrent = true
+                current = ""
+            }
+        } else {
+            if shouldAddToCurrent {
+                current += String(char)
+            }
+        }
+    }
+    
+    return result
 }
